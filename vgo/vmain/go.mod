f7e2516db4578ce19527c778d394691ee32f76e1
Ivan Korolev <i.d.korolev@gmail.com>
2018-06-20 13:01:55 +0300

    vmain: check foo-bar dependecy handling

--- vgo/vmain/go.mod
+++ vgo/vmain/go.mod
@@ -1,6 +1,7 @@
 module github.com/mwf/goplay/vgo/vmain
 
 require (
+	github.com/mwf/goplay/vgo/vtest/bar v0.0.0-20180620095244-cf1787f64386
 	github.com/peterbourgon/vtest/v2 v2.0.0
 	github.com/peterbourgon/vtest/v3 v3.0.3
 )
--- vgo/vmain/main.go
+++ vgo/vmain/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"github.com/mwf/goplay/vgo/vtest/bar"
+	"github.com/mwf/goplay/vgo/vtest/foo"
+
 	vtest2 "github.com/peterbourgon/vtest/v2"
 	foo2 "github.com/peterbourgon/vtest/v2/foo"
 	vtest3 "github.com/peterbourgon/vtest/v3"
@@ -10,4 +13,8 @@ import (
 func main() {
 	println("vtest2", vtest2.Version, "foo2", foo2.Version)
 	println("vtest3", vtest3.Version, "foo3", foo3.Version)
+
+	println("bar", bar.Bar)
+	println("foobar", bar.FooBar)
+	println("foo", foo.Foo)
 }
